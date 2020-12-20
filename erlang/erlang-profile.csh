486307101d5c6e596a74cad63b0ce265aa39a86b
Rafael Zalamena <rzalamena@gmail.com>
2020-12-20 12:10:58 -0300

    erlang: new slackbuild
    Add slackbuild for erlang.

---
+++ erlang/erlang-profile.csh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Add erlang to path so we can find man pages.
+setenv MANPATH="$MANPATH:/usr/lib/erlang/man"
---
+++ erlang/erlang-profile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Add erlang to path so we can find man pages.
+export MANPATH="$MANPATH:/usr/lib/erlang/man"
