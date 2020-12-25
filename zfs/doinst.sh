ba49412477fae804f1c565a42f3efb95d89f2bd6
Rafael Zalamena <rzalamena@gmail.com>
2020-12-25 11:19:59 -0300

    zfs: new slackbuild

---
+++ zfs/doinst.sh
@@ -0,0 +1,32 @@
+update() {
+  new_file="$1"
+  old_file=$(echo "$new_file" | sed 's/\.new$//')
+
+  if [ ! -f $old_file ]; then
+    # Old file doesn't exist, then just move it over.
+    mv $new_file $old_file
+  else
+    old_md5=$(md5sum $old_file | cut -d ' ' -f 1)
+    new_md5=$(md5sum $new_file | cut -d ' ' -f 1)
+    # Same file, just remove the new one.
+    if [ "$old_md5" == "$new_md5" ]; then
+      rm $new_file
+    fi
+  fi
+
+  # Keep new file so admin is asked what to do.
+}
+
+update_with_perms() {
+  new_file="$1"
+  old_file=$(echo "$new_file" | sed 's/\.new$//')
+
+  if [ -f $old_file ]; then
+    old_perms=$(stat -c '%a' "$old_file")
+    chmod $old_perms $new_file
+  fi
+
+  update $new_file
+}
+
+update_with_perms '/etc/rc.d/rc.zfs.new'
