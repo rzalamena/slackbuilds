09d913d663e6a780831e04a353f6a905887ea35d
Rafael Zalamena <rzalamena@gmail.com>
2021-07-31 10:37:20 -0300

    nvidia-kernel: import slackbuild
    New slackbuild for building nvidia kernel.

---
+++ nvidia-kernel/doinst.sh
@@ -0,0 +1,16 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config usr/share/X11/xorg.conf.d/99-nvidia-drm-outputclass.conf.new
+
+chroot . /sbin/depmod -a @KERN@ 2>/dev/null
