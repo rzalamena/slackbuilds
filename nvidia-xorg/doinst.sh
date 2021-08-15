379a8b58d77da99d2b23d55adf6457332b1d975b
Rafael Zalamena <rzalamena@gmail.com>
2021-08-15 08:49:23 -0300

    nvidia-xorg: fix bugs
    Correct the application path and regenerate desktop application database
    to immediately show up.

--- nvidia-xorg/doinst.sh
+++ nvidia-xorg/doinst.sh
@@ -12,3 +12,7 @@ config() {
 }
 
 config usr/share/X11/xorg.conf.d/99-nvidia-drm-outputclass.conf.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
